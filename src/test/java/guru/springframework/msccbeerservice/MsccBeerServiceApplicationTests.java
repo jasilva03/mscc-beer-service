9d23ba1f4a3b0ed135497053efc943e29d311d92
Ariel Silva <jasilva03@yahoo.com.ar>
2020-09-03 12:54:11 -0600

    Initial commit

---
+++ src/test/java/guru/springframework/msccbeerservice/MsccBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msccbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsccBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
